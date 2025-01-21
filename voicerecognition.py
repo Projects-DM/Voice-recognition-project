2462cc95923a4096f3d451df089307ee5540cb89
DigitalMete <digitalmentedm@gmail.com>
2025-01-21 17:47:35 -0500

    This code corresponds to installing dependencies and the initial setup.

---
+++ voicerecognition.py
@@ -0,0 +1,9 @@
+# Install SpeechRecognition with pip install speech_recognition
+# Install pyaudio module using pip install pyaudio
+# pip install setuptools
+# pip install pyaudio
+# pip install pyttsx3
+
+import speech_recognition as sr
+import webbrowser
+import pyttsx3  
