b18c3911040d993ce1a0da70b29b77a5e0c37687
DigitalMete <digitalmentedm@gmail.com>
2025-01-21 18:10:55 -0500

    The talk() function captures the user's speech using a microphone.

--- voicerecognition.py
+++ voicerecognition.py
@@ -10,3 +10,14 @@ import pyttsx3
 
 recognizer = sr.Recognizer()   
 engine = pyttsx3.init()
+
+def talk(): 
+    mic = sr.Microphone()  
+    with mic as source:
+
+        audio = recognizer.listen(source)
+
+    text = recognizer.recognize_google(audio, language='ES')
+
+    print(f"Has dicho: {text}")
+    return text.lower()   
